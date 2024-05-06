2604cd8f7ed87ff34760e75edf1407c906b2c742
Taeer Bar-Yam <taeer.bar-yam@moduscreate.com>
2024-05-06 19:43:30 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Mucking around with zig stuff";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zls-flake = {
+      url = "github:zigtools/zls";
+      inputs.zig-overlay.follows = "zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, zig-overlay, zls-flake }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    lib = nixpkgs.lib;
+    zig = zig-overlay.packages.${system}.master;
+    zls = zls-flake.packages.${system}.zls;
+  in {
+    packages.${system} = {
+      inherit zig;
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ zig zls ];
+    };
+  };
+}
